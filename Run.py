9bc96ccb4311adcf871d754a14119796947b3350
Grigorii Raikov <griray090494@gmail.com>
2020-08-17 14:14:16 +0300

    Add query, adding db

--- Run.py
+++ Run.py
@@ -1,5 +1,37 @@
-from models import Publisher, Session
+from sqlalchemy.orm import Query
+
+from models import Publisher, Book, Shop, Session
+
+Session
 
 if __name__ == '__main__':
     session = Session()
-    publisher = Session.query(Publisher)
+    publish = session.query(Publisher)
+    book = session.query(Book)
+    shop = session.query(Shop)
+    q = Query([Publisher, Book, Shop], session=Session)
+
+
+    publ1 = Publisher(name='Foma'),
+    publ2 = Publisher(name='EKSMO'),
+    publ3 = Publisher(name='Key'),
+    publ4 = Publisher(name='Train'),
+
+    book1 = Book(name='Dorian Grey'),
+    book2 = Book(name='Harry Potter'),
+    book3 = Book(name='Dream'),
+    book4 = Book(name='Perfume'),
+    book5 = Book(name='Night in Lisbon'),
+    book6 = Book(name='3 Fellows'),
+    book7 = Book(name='Post'),
+    book8 = Book(name='Kingdom'),
+
+    shop1 = Shop(name='Mir Knig'),
+    shop2 = Shop(name='Dom Knigi'),
+    shop3 = Shop(name='BookBerry'),
+
+    session.add_all([publ1, publ2, publ3, publ4, book1, book2, book3, book4, book5, book6, book7, book8, shop1, shop2,
+                     shop3])
+
+    
+
--- models.py
+++ models.py
@@ -2,57 +2,42 @@ import sqlalchemy as sq
 from sqlalchemy import sql
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
+import psycopg2
 
-engine = sq.create_engine('')
+engine = sq.create_engine('postgresql+psycopg2://postgres:postgres@localhost:5439//Alchemy')
 Session = sessionmaker(bind=engine)
 Session.configure(bind=engine)
 Base = declarative_base()
 
-publisher_book = sq.Table('publisher_book', Base.metadata,
-                          sq.Column('publisher_id', sq.Integer, sq.ForeignKey('Publisher.id')),
-                          sq.Column('book_id', sq.Integer, sq.ForeignKey('Book.id'))
-                          )
-
 
 class Publisher(Base):
-    __tablename__ = 'Publisher'
+    __tablename__ = 'publisher'
 
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String)
-
-
-book_stock = sq.Table('book_stock', Base.metadata,
-                      sq.Column('book_id', sq.Integer, sq.ForeignKey('Book.id')),
-                      sq.Column('stock_id', sq.Integer, sq.ForeignKey('Stock.id'))
-                      )
-
-stock_shop = sq.Table('stock_shop', Base.metadata,
-                      sq.Column('stock_id', sq.Integer, sq.ForeignKey('Stock.id')),
-                      sq.Column('shop_id', sq.Integer, sq.ForeignKey('Shop.id'))
-                      )
 
 
 class Book(Base):
-    __tablename__ = 'Book'
+    __tablename__ = 'book'
 
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String)
-    id_publisher = relationship('Publisher', secondary=publisher_book, back_populates='publishers')
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey('publisher.id'))
 
 
 class Shop(Base):
-    __tablename__ = 'Shop'
+    __tablename__ = 'shop'
 
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String)
 
 
 class Stock(Base):
-    __tablename__ = 'Stock'
+    __tablename__ = 'stock'
 
     id = sq.Column(sq.Integer, primary_key=True)
-    id_book = relationship('Book', secondary=book_stock, back_populates='books')
-    id_shop = relationship('Shop', secondary=stock_shop, back_populates='shops')
+    id_book = sq.Column(sq.Integer, sq.ForeignKey('book.id'))
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey('shop.id'))
     count = sq.Column(sq.Integer)
 
 
@@ -63,10 +48,10 @@ sale_stock = sq.Table('sale_stock', Base.metadata,
 
 
 class Sale(Base):
-    __tablename__ = 'Sale'
+    __tablename__ = 'sale'
 
     id = sq.Column(sq.Integer, primary_key=True)
     price = sq.Column(sq.Integer)
     date_sale = sq.Column(sq.Date)
-    id_stock = relationship('Stock', secondary=sale_stock, back_populates='stocks')
+    id_stock = relationship('stock', secondary=sale_stock, back_populates='stocks')
     count = sq.Column(sq.Integer)
